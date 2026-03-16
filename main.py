58df9689749ec3eb874f19830cae1ca8560ce5f9
Csaba Meszaros <csabametzg@gmail.com>
2026-03-16 18:23:35 +0100

    Add REST API client

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.utils.logger import setup_logger
 from src.database.schema import create_tables
+from src.api.client import fetch_orders
 
 logger = setup_logger()
 
 logger.info("Pipline started")
 
 create_tables()
-
 logger.info("Database initialized")
+
+orders = fetch_orders()
+logger.info(f"Orders received: {len(orders)}")
---
+++ src/api/client.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+WC_BASE_URL = os.getenv("ENV_WC_BASE_URL")
+WC_CONSUMER_KEY = os.getenv("ENV_WC_CONSUMER_KEY")
+WC_CONSUMER_SECRET = os.getenv("ENV_WC_CONSUMER_SECRET")
+
+# BASE_URL = "https://beautyland.peakfunneldesign.com"
+# WC_CONSUMER_KEY = "ck_14a454568d42f1eb331c8a48cf12a0e162eebc8c"
+# WC_CONSUMER_SECRET = "cs_4a09195527204940d75722c89f3c1c280b55f25e"
+
+def fetch_orders():
+    logger.info("Fetching orders from API")
+
+    if not WC_BASE_URL:
+        raise ValueError("WC_BASE_URL is missing from .env file")
+
+    if not WC_CONSUMER_KEY or not WC_CONSUMER_SECRET:
+        raise ValueError("WooCommerce API credentials are missing from .env file")
+
+    url = f"{WC_BASE_URL}/wp-json/wc/v3/orders"
+
+
+    response = requests.get(
+        url,
+        auth=(WC_CONSUMER_KEY, WC_CONSUMER_SECRET),
+        params={"per_page": 20},
+        timeout=30
+    )
+
+    if response.status_code != 200:
+        raise Exception(f"API request failed: {response.status_code} - {response.text}")
+
+    data = response.json()
+    logger.info(f"Fetched {len(data)} orders")
+
+    return data
